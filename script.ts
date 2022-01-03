1d19ade6051e3ea2946d9fcd6e5fef49a7c0155c
Dodkes <84540150+Dodkes@users.noreply.github.com>
2022-01-03 02:40:22 +0100

    First update
    Randomly creating stars algorithm done

--- script.ts
+++ script.ts
@@ -1,15 +1,13 @@
-const numberStars = 50
+const numberStars: number = 50
 const sky = document.getElementById('sky')
-let stars: {width: number, height: number, posX: number, posY: number}[] = []
+let stars: {size: number, posX: number, posY: number}[] = []
 
 class Star{
-    width: number
-    height: number
+    size: number
     posX: number
     posY: number
-    constructor(width: number, height: number, posX: number, posY: number){
-        this.width = width
-        this.height = height
+    constructor(size: number, posX: number, posY: number){
+        this.size = size
         this.posX = posX
         this.posY = posY
     }
@@ -19,13 +17,35 @@ class Star{
 for (let i: number = 0; i < numberStars; i++) {
     let randomSize: number = Math.floor(Math.random() * 3 + 1)
     let randomPosX: number = Math.floor(Math.random() * 100)
-    let randomPosY: number = Math.floor(Math.random() * 100)
-    stars.push(new Star(randomSize, randomSize, randomPosX, randomPosY))
+    let randomPosY: number = Math.floor(Math.random() * 80)
+    stars.push(new Star(randomSize, randomPosX, randomPosY))
 }
 
 //star creation based on objects loop
 for (let j: number = 0; j < stars.length; j++) {
-    let star = document.createElement('div')
-    sky.appendChild(star)
-    star.classList.add('big')
+    let star: HTMLElement = document.createElement('div')
+    createStars(star, j)
+    sizingStars(star, j)
+}
+
+function createStars(myStar: HTMLElement, i: number){
+    sky.appendChild(myStar)
+    myStar.style.backgroundColor = 'white'
+    myStar.classList.add('big') //docasne
+    myStar.style.left = stars[i].posX + '%'
+    myStar.style.top = stars[i].posY + 'vh'
+}
+
+function sizingStars(myStar: HTMLElement, i: number){
+    switch(stars[i].size){
+        case 1:
+            console.log('1')
+            break
+        case 2:
+            console.log('2')
+            break
+        case 3:
+            console.log('3')
+            break
+    }
 }
