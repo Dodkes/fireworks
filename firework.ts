805274cda73d93516c6c7ceadbdc9a5c80e91597
Jozef Zitt <84540150+Dodkes@users.noreply.github.com>
2022-01-06 06:47:41 +0100

    1st firework
    After struggling.. finally set up firework loop function, and hence we have 1st firework here. This is just 1st version of the firework, will be stylized and animated later.

--- firework.ts
+++ firework.ts
@@ -1,23 +1,52 @@
 const mySvg = document.querySelector('svg')
-const fireworkDots: number = 1 
+const fireworkDots: number = 80 
 let stringX: string
 let stringY: string
 let stringR: string
+let circles: SVGCircleElement[] = []
 
-for(let k = 0; k < fireworkDots; k++) {
-    let circle = document.createElementNS('http://www.w3.org/2000/svg', 'circle')
-    attribute(circle, 505, 500, 10, 'rgba(0, 255, 255, 0.5)')
+let posX: number = 500
+let posY: number = 300
+
+fireworkLoop(2, 2)
+fireworkLoop(-2, -2)
+fireworkLoop(-2, 2)
+fireworkLoop(2, -2)
+
+fireworkLoop(0, -2.8)
+fireworkLoop(0, 2.8)
+fireworkLoop(-2.8, 0)
+fireworkLoop(2.8, 0)
+
+fireworkLoop(2.5, 1)
+fireworkLoop(1, 2.5)
+fireworkLoop(-1, 2.5)
+fireworkLoop(1, -2.5)
+fireworkLoop(1, 2.5)
+
+fireworkLoop(-1, -2.5)
+fireworkLoop(-2.5, 1)
+fireworkLoop(-2.5, -1)
+fireworkLoop(2.5, -1)
+
+function fireworkLoop(whereX: number, whereY: number){
+    for (let k = 0; k < fireworkDots; k++) {
+        posX+= whereX
+        posY+= whereY
+        circles[k] = document.createElementNS('http://www.w3.org/2000/svg', 'circle')
+        attribute(circles[k], posX, posY, 4, 'rgba(0, 255, 255, 1)')
+    }
+    posX = 500
+    posY = 300
 }
-
-
 
 function attribute(element: SVGCircleElement, posX: number, posY: number, radius: number, color: string){
     mySvg.appendChild(element)
-    if(typeof(posX) === 'number') { //TS dont allow us to assign number to string numbers
+    if (typeof(posX) === 'number') { //TS dont allow us to assign number to string numbers
         stringX = posX.toString()
         stringY = posY.toString()
         stringR = radius.toString()
-    }  
+    } 
     element.setAttribute('cx', stringX)
     element.setAttribute('cy', stringY)
     element.setAttribute('r', stringR)
